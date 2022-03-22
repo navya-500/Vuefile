a04502843d4017258fabf67d33c9e6156f3adb85
navya-500 <navya.doddi@500apps.com>
2022-03-22 19:18:07 +0530

    importfile

---
+++ src/First.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    iam here
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
---
+++ src/second.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    hello World
+    welcome to vue 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  
+}
+</script>
