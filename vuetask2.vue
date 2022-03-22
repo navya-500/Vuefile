8d57cfbf166863164a32518025e48a31129d612a
navya-500 <navya.doddi@500apps.com>
2022-03-22 15:00:42 +0530

    vuetask

---
+++ vuetask2.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<p>vue.js</p>
+<p>hello world</p>
+</template>
+<script>
+var name = "navya"
+export default {
+    name:example,
+    data(){
+        return {
+            firstname:"navya",
+            lastname:"doddi",
+            fullname:" "
+        }
+    },
+    created(){
+        console.log(this.firstname);
+    }
+    mounted(){
+        this.fullname = this.firstname+this.lastname
+        console.log(this.fullname)
+    }
+    methods:{
+        
+    }
+}
